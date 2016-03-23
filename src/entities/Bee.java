8f177b78d6a5b98130967c07d7558668eb49616d
1485246 <1485246@li-h008-0t29>
2016-03-23 12:09:20 -0400

    Merge origin/master
    Conflicts:
    	src/ca/qc/bdeb/info204/gameEngine/Model.java

--- src/entities/Bee.java
+++ src/entities/Bee.java
@@ -5,6 +5,7 @@
  */
 package entities;
 
+import maps.MiniMap;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -12,32 +13,86 @@ import org.newdawn.slick.SpriteSheet;
 
 /**
  *
- * @author Olivier
+ * @author 1485246
  */
 public class Bee extends Mob {
 
-    
+    private final float speed = 0.2f;
+    private int comptDirection = 0;
+    private MiniMap map;
+    private boolean rndMove;
+
+    public Bee(MiniMap map) {
+        this.moveAnimations = new Animation[4];
+        this.map = map;
+        this.direction = 0;
+    }
+
     @Override
     public void update(int delta) {
+        this.x = futurX(delta);
+        this.y = futurY(delta);
     }
 
     @Override
     public void init() throws SlickException {
-        moving = false;
-        moveAnimations = new Animation[4];
-        this.x = 100;
-        this.y = 100;
-        SpriteSheet moveSpriteSheet = new SpriteSheet("res/sprites/bee.png", 64, 64);
+        this.x = 564;
+        this.y = 154;
+        SpriteSheet moveSpriteSheet = new SpriteSheet("res/sprites/bee.png", 32, 32);
         for (int i = 0; i < 4; i++) {
             this.moveAnimations[i] = loadAnimation(moveSpriteSheet, 0, 3, i);
         }
-        
     }
 
     @Override
     public void render(Graphics g) throws SlickException {
-        g.drawAnimation(moveAnimations[2], x, y);
+        g.drawAnimation(moveAnimations[direction], x, y);
     }
-    
-    
+
+    // Teste la position pour se déplacer en X
+    private float futurX(int delta) {
+        float futurX = this.x;
+        if (rndMove) {
+            switch (this.direction) {
+                case 1:
+                    futurX = this.x - speed * delta;
+                    comptDirection += 1;
+                    if (comptDirection % 300 == 0) {
+                        this.direction = 2;
+                    }
+                    break;
+                case 3:
+                    futurX = this.x + speed * delta;
+                    comptDirection += 1;
+                    if (comptDirection % 300 == 0) {
+                        this.direction = 0;
+                    }
+                    break;
+            }
+        }
+        return futurX;
+    }
+
+    // Teste la position pour se déplacer en Y
+    private float futurY(int delta) {
+        float futurY = this.y;
+        switch (this.direction) {
+            case 0:
+                futurY = this.y + speed * delta;
+                comptDirection += 1;
+                if (comptDirection % 300 == 0) {
+                    this.direction = 1;
+                }
+                break;
+            case 2:
+                futurY = this.y - speed * delta;
+                comptDirection += 1;
+                if (comptDirection % 300 == 0) {
+                    this.direction = 3;
+                }
+                break;
+        }
+        return futurY;
+    }
+
 }
