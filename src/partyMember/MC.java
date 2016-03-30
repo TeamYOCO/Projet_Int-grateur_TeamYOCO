bb078c81f3fa5df4c790e08ba1eec251fedb9ace
Tomas <Tomas@Shikazuma>
2016-03-30 12:24:29 -0400

    Merge origin/master
    Conflicts:
    	src/entities/Bee.java

---
+++ src/partyMember/MC.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partyMember;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Tomas
+ */
+public class MC extends PartyMember {
+
+    public MC(int hpMax, int hp, int mpMax, int mp, int attack, int speed, int specialAttack, int defence, int specialDefence, int expNeeded, int exp, ArrayList skillSet) {
+        super(hpMax, hp, mpMax, mp, attack, speed, specialAttack, defence, specialDefence, expNeeded, exp, skillSet);
+        
+    }
+
+   
+}
