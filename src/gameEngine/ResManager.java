093b4a721a1986f4b227dfc3b24914f5d873aee1
1455367 <1455367@li-h008-0t30>
2016-04-13 13:27:08 -0400

    Merge origin/master
    Conflicts:
    	src/statesOfGame/CombatScreen.java
    	src/statesOfGame/MainMenu.java

--- src/gameEngine/ResManager.java
+++ src/gameEngine/ResManager.java
@@ -10,18 +10,6 @@ package gameEngine;
  * @author 1455367
  */
 public class ResManager {
-    private static ResManager instance = null;
-    
-    protected ResManager(){
-        
-    }
-    
-    public static ResManager getInstance(){
-        if (instance == null){
-            instance = new ResManager();
-        }
-        return instance;
-    }
     
     
 }
